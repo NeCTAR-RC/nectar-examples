df6a1d76486173b4af596564f70e7bdffecb40d1
Martin Paulo <martin.paulo@gmail.com>
2014-06-11 12:17:01 +1000

    Added ability to stop the server

--- start.sh
+++ start.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-# make the project
-mvn package
-# and run it...
-mvn exec:java -Dexec.mainClass=nz.co.paulo.Mainstay
+# create a file to record the pid in
+basePath=$("pwd")
+pidFile="${basePath}/pids.pid"
+
+if [ -f ${pidFile} ];
+then
+  echo "$pidFile already exists. Stop the process before attempting to start."
+else
+    # make the project
+    mvn package
+
+    # and run it...
+    mvn exec:java -Dexec.mainClass=nz.co.paulo.Mainstay &
+
+    # save the pid
+    echo -n "$! " >> ${pidFile}
+fi
