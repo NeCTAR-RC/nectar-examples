4f2c7fb66d4df1c202b768bba44812422b0183b0
Martin Paulo <martin.paulo@gmail.com>
2014-07-02 14:59:58 +1000

    Rejigged to use Mustache, and to allow both a form post and a regular post to trigger an alarm

---
+++ src/main/java/nz/co/paulo/History.java
@@ -0,0 +1,39 @@
+package nz.co.paulo;
+
+import spark.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Martin Paulo on 1/07/2014.
+ * I guess this is an implementation of the Presentation Model pattern
+ * (http://martinfowler.com/eaaDev/PresentationModel.html). It's simply used to transfer values into the
+ * Mustache template.
+ */
+public class History {
+
+    private int rowCount;
+    List<Row> rows;
+
+    static class Row {
+        String alert, value, cssClass;
+        Row(String alert, String value, String cssClass) {
+            this.alert = alert;
+            this.value = value;
+            this.cssClass = cssClass;
+        }
+    }
+
+    public ModelAndView getHistory(Map<String, Integer> alarmTotals) {
+        rowCount = 0;
+        rows = new ArrayList();
+        alarmTotals.forEach( (key, value)-> rows.add(new Row(key, value.toString(),getCssClass(rowCount++))));
+        return new ModelAndView(this, "history.mustache");
+    }
+
+    private String getCssClass(int rowCount) {
+        return ((rowCount % 2 != 0) ? "even" : "odd");
+    }
+}
--- src/main/java/nz/co/paulo/Mainstay.java
+++ src/main/java/nz/co/paulo/Mainstay.java
@@ -1,60 +1,57 @@
 package nz.co.paulo;
 
+import spark.ModelAndView;
 import spark.Request;
-import spark.Route;
+import spark.Response;
+import spark.template.mustache.MustacheTemplateEngine;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 import static spark.Spark.*;
 
 /**
- * An application for me to learn a little about Spark, and a little bit about ceilometer alarms
+ * An application to learn a little about Spark, and a little bit about ceilometer alarms
  * Created by Martin Paulo on 19/05/2014.
  */
 public class Mainstay {
 
-    private static final Map<String, Integer> totals = new ConcurrentHashMap<>();
+    private static final Map<String, Integer> totals = new ConcurrentSkipListMap();
 
     public static void main(String[] args) {
         // we'll run on port 8080
         setPort(8080);
-        // we'll serve a css file from here
+        // we'll serve a css file from here, as well as the home page
         staticFileLocation("/public");
-        // alarms will be of the form:
-        // http://www.somesite.com/alarm?source=some_value
-        // where source and some_value can vary
-        get("/alarm", addToAlarmTotals());
-        // and we can view the history of our alarms at http://www.somesite.com/history
-        get("/history", (request, response) -> new HistoryPage(totals).toString());
-        // and we now want to check what happens on a post...
-        post("/alarm", newAlarmSounded());
+        // we will allow users to register an alarm via a form
+        get("/alarm", (rq, rs) -> new ModelAndView(new HashMap(), "form.mustache"), new MustacheTemplateEngine());
+        // hence we want to handle the form post...
+        post("/alarm", (rq, rs) -> alarmFromForm(rq, rs));
+        // and the actual alarms themselves...
+        post("alarm/:name", (rq, rs) -> alarmRegistered(rq, rs));
+        // and we can view the history of our alarms
+        get("/history", (rq, rs) -> new History().getHistory(totals), new MustacheTemplateEngine());
     }
 
-    private static Route addToAlarmTotals() {
-        return (request, response) -> {
-            String source = recordAlarm(request);
-            // just how do we respond to a ceilometer alarm?
-            return "Hello World! This is lambda responding to your alarm: " + source + "...";
-        };
+    private static Response alarmRegistered(Request request, Response response) {
+        countAlarm(request.params(":name"));
+        response.status(HttpServletResponse.SC_OK);
+        return response;
     }
 
-    private static String recordAlarm(Request request) {
-        String source = request.queryParams("source");
-        // we got a request that doesn't match our simple scheme...
-        if (source == null) {
-            source = "unknown?";
+    private static Response alarmFromForm(Request request, Response response) {
+        countAlarm(request.queryParams("alarm_name"));
+        response.redirect("/history");
+        return response;
+    }
+
+    private static void countAlarm(String source) {
+        if (source == null || source.length() <= 0) {
+            source = "unknown alarm?";
         }
         totals.put(source, totals.getOrDefault(source, 0) + 1);
-        return source;
-    }
-
-    private static Route newAlarmSounded() {
-        return (request, response) -> {
-            recordAlarm(request);
-            response.status(200);
-            return response;
-        };
     }
 
 }
