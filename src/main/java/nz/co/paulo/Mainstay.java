99d7ae12f1d15a66efb11010adbae827d0c303ba
Martin Paulo <martin.paulo@gmail.com>
2014-07-08 10:24:07 +1000

    Added more monitoring.

--- src/main/java/nz/co/paulo/Mainstay.java
+++ src/main/java/nz/co/paulo/Mainstay.java
@@ -5,7 +5,11 @@ import spark.Request;
 import spark.Response;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static spark.Spark.*;
@@ -18,12 +22,13 @@ public class Mainstay {
 
     private static ProcessBuilder pb;
     private static Process process;
+    private static String[] command = {"stress", "--cpu 1", "-timeout 60"};
 
     public static void main(String[] args) {
         pb = new ProcessBuilder();
         pb.redirectErrorStream(true);
         // http://linux.die.net/man/1/stress
-        pb.command("stress", "--cpu 1", "--timeout 60");
+        pb.command(command);
 
         // we'll run on port 8080
         setPort(8080);
@@ -40,8 +45,24 @@ public class Mainstay {
             try {
                 System.out.println("Starting...");
                 process = pb.start();
+                InputStream is = process.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                System.out.printf("Output of running %s is:\n",
+                        Arrays.toString(command));
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                //Wait to get exit value
+
+                    int exitValue = process.waitFor();
+                    System.out.println("\n\nExit Value is " + exitValue);
             } catch (IOException e) {
                 process = null;
+                e.printStackTrace();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 //            process.waitFor(1, TimeUnit.SECONDS);
